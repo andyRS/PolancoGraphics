d27d4d774b52da29d50e8666af5acd7d002a5e05
andyRS <andy337@hotmail.es>
2021-12-28 17:50:13 -0400

    Add files via upload

--- js/jquery-menu.js
+++ js/jquery-menu.js
@@ -1,46 +1,56 @@
-$(document).ready(function (){
-    $(window).scroll(function () {
-        scroll = $(window).scrollTop();
-        if (scroll > 580) {
-          var menu = $(".nav");
-          menu.css({ position: "fixed" });
-          menu.css({ width: "100%" });
-          menu.css({ height: "115px" });
-          menu.css({ transition: "all 300ms" });
-          menu.css({ background: "#050716" });
-          $(".nav a").css({ color: "#fff" });
-          menu.css({ boxShadow: "rgba(0, 0, 0, 0.22) 6px 1px 1px" });
-          menu.css({ "z-index": "100" });
-          if(scroll > 580){
-            var active = document.getElementsByClassName(' active')
-            menu.css({ borderBottom: "1px solid #fff" });
-            var menuLinks = $('.nav-menu-link');
-                menuLinks.css("font-size","15px");
-                
-                var redes = $('.social-body');
-                redes.show();
-          }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    scroll = $(window).scrollTop();
+    var mediaqueryList = window.matchMedia("(min-width: 1350px)");
+    var mediaqueryList2 = window.matchMedia("(min-width: 1278px)");
+    
+    var menu = $(".nav");
+    var menuUl = $(".nav-menu");
+      if (mediaqueryList.matches && mediaqueryList2.matches && scroll > 580) {
+        menu.css({ position: "fixed" });
+        menu.css({ width: "100%" });
+        menu.css({ height: "115px" });
+        menu.css({ transition: "all 300ms" });
+        menu.css({ background: "#050716" });
+        menuUl.css("left","0");
+        menuUl.css("top","0");
+      $(".nav a").css({ color: "#fff" });
+      menu.css({ boxShadow: "rgba(0, 0, 0, 0.22) 6px 1px 1px" });
+      menu.css({ "z-index": "100" });
+      if (scroll > 580) {
+        var active = document.getElementsByClassName(' active')
+        menu.css({ borderBottom: "1px solid #fff" });
+        var menuLinks = $('.nav-menu-link');
+        menuLinks.css("font-size", "15px");
 
-          var services = $('#services');
-          services.css("top","619px");
+        var redes = $('.social-body');
+        redes.show();
+      }
+
+      var services = $('#services');
+      services.css("top", "619px");
 
 
-          } 
-        else {
-            var menu = $(".nav");
-            var menuLinks = $('.nav-menu-link');
-            menuLinks.css("font-size","18px");
-            menu.css({ borderBottom: "none" });
-            menu.css({ transition: "all 300ms" });
-            menu.css({ position: "relative" });
-          $(".nav").css({ position: "relative" });
-          $(".nav").css({ background: "transparent" });
-          $(".nav").css({ "box-shadow": "0 0 0" });
-            var redes = $('.social-body');
-                redes.hide();
-        
-        }
+    }
+    else {
+      var menu = $(".nav");
+      var menuLinks = $('.nav-menu-link');
+      menuLinks.css("font-size", "18px");
+      menu.css({ borderBottom: "none" });
+      menu.css({ transition: "all 300ms" });
+      menu.css({ position: "relative" });
+      menuUl.css("left","630px");
+      $(".nav").css({ position: "relative" });
+      $(".nav").css({ background: "transparent" });
+      $(".nav").css({ "box-shadow": "0 0 0" });
 
-        
-    })
+      var redes = $('.social-body');
+      redes.hide();
+
+    }
+    
+    //
+
+
+  })
 })
--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-window.addEventListener('load',function(){
-    
+window.addEventListener('load', function () {
+
     /** Indicador de menu*/
 
     const secciones = document.querySelectorAll('.section')
     const menuItems = document.querySelectorAll('.nav-menu-item')
 
-const funcionObserverser = entries =>{
-    entries.forEach(entry => {
-        if(entry.isInteresting){
-            const itemActual = Array.from(menuItems).find(item => item.dataset.url === entry.target.id)
-            itemActual.classList.add('active')
-        }
-    });
-}
+    const funcionObserverser = entries => {
+        entries.forEach(entry => {
+            if (entry.isInteresting) {
+                const itemActual = Array.from(menuItems).find(item => item.dataset.url === entry.target.id)
+                itemActual.classList.add('active')
+            }
+        });
+    }
 
     const observer = new IntersectionObserver(funcionObserverser, {
         root: null,
@@ -20,8 +20,31 @@ const funcionObserverser = entries =>{
         threshold: 0.8
     })
 
-    secciones.forEach(seccion =>observer.observe(seccion))
+    secciones.forEach(seccion => observer.observe(seccion))
 
 
-})
+    //Menu responsivo
+
+    const iconoMenu = document.querySelector('.menu-responsivo-icono');
+     menu = document.querySelector('#menu-responsivo');
+     
+
+    iconoMenu.addEventListener('click', (e) => {
+        menu.classList.toggle('active');
+
+        document.body.classList.toggle('opacity');
+
+        const rutaActual = e.target.getAttribute('src');
+
+        if(rutaActual == imagenes/menu-responsive.png){
+            e.target.setAttribute('src','imagenes/menu2-responsivo.png');
+        }else{
+            e.target.setAttribute('src','imagenes/menu-responsivo.png');
+        }
+
+
+
+    });
+
+});
 
